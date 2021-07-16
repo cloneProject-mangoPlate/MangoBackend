3cd0281061526c290faa67eac7a97aa375f51077
dennis <85466642+dennis9352@users.noreply.github.com>
2021-07-16 20:54:12 +0900

    import err
    importing modules err fixed

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 import express from "express";
-import auth from "../middlewares/auth-middleware.js";
+// import auth from "../middlewares/auth-middleware.js";
 import restaurantsRouter from "./restaurants.js";
-import reviewsRouter from "./reviews";
-import usersRouter from "./users";
+import reviewsRouter from "./reviews.js";
+import usersRouter from "./users.js";
 
 const router = express.Router();
 
 router.use("/retaurants", restaurantsRouter);
 router.use("/reviews", reviewsRouter);
 router.use("/users", usersRouter);
-router.use(auth);
+// router.use(auth);
 
 
 export default router;
