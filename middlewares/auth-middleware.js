e304bc6752e33ac16ba002b10f0f5b2bbb26d3c2
soujinko <soujinko@naver.com>
2021-07-20 21:38:29 +0900

    social signin

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -4,6 +4,55 @@
 
 
 
+import User from "../models/user.js";
+import request from "request";
 
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+  const [tokenType, tokenValue] = authorization.split(" ");
+
+  if (tokenType !== "Bearer") {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요",
+    });
+    return;
+  }
+
+  try {
+    const options = {
+      uri: "https://kapi.kakao.com/v1/user/access_token_info",
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    request(options, function (err, response, body) {
+      //callback
+    });
+
+    request.get(
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        url: "https://kapi.kakao.com/v1/user/access_token_info",
+      },
+      function (error, response, body) {
+        console.log(response);
+        if (response) {
+          res.locals.userId = id;
+          next();
+        } if(error){
+          throw 
+        }
+        // const { id } = body;
+        // console.log(id);
+      }
+    );
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      res.status(419).send({ message: "token 만료" });
+      return;
+    } else {
+      res.status(401).send({ message: "token이 유효하지 않습니다." });
+      return;
+    }
+  }
+};
 
 
--- routes/socialSignin.js
+++ routes/socialSignin.js
@@ -1,13 +1,83 @@
 import express from "express";
+import User from "../models/user.js";
+import passport from "passport";
+import request from "request";
+
+// strategy import
+import Kakao from "passport-kakao";
+
 const router = express.Router();
+router.use(passport.initialize());
 
+const KakaoStrategy = Kakao.Strategy;
 
+// router.get("/login", function (req, res) {
+//   res.render("login");
+// });
 
+// 카카오로그인
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
+passport.use(
+  "kakao",
+  new KakaoStrategy(
+    {
+      clientID: "88806fd7f63dd24d3065af028f601b16",
+      callbackURL: "/api/social/kakao/callback", // 위에서 설정한 Redirect URI
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const user = profile._json.kakao_account;
+      // user.provider = profile.provider;
+      // req.session.save(function(err){
 
+      // })
+      user.token = accessToken;
+      console.log("토큰!", accessToken);
+      console.log("프로필이거", profile);
+      done(null, user);
+    }
+  )
+);
 
+function authSuccess(req, res) {
+  const { profile, email, token } = req.user;
+  let user = profile.nickname;
+  console.log("social login test", user, email, token);
+  request.post(
+    {
+      headers: { Authorization: `Bearer ${token}` },
+      url: "https://kapi.kakao.com/v1/api/talk/profile",
+      json: true,
+    },
+    async function (error, response, body) {
+      const { profileImageURL } = body;
+      console.log(profileImageURL);
 
+      const myuser = await User.findOne({
+        userName: user,
+        email: email,
+      });
+      if (!myuser) {
+        await User.create({
+          userName: user,
+          email: email,
+        });
+        res.send({ profileImageURL, user, token });
+      } else {
+        res.send({ profileImageURL, user, token });
+      }
+    }
+  );
+}
+
+router.get("/kakao", passport.authenticate("kakao"));
+router.get("/kakao/callback", passport.authenticate("kakao"), authSuccess);
 
 export default router;
