8aca7bb8da29b994647534079571ac03aa948a8f
soujinko <soujinko@naver.com>
2021-07-21 14:23:46 +0900

    shop api add

--- routes/shops.js
+++ routes/shops.js
@@ -3,6 +3,8 @@ import Shop from "../models/shop.js";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {});
+
 router.get("/:keyword", async (req, res) => {
   try {
     const { keyword } = req.params;
@@ -11,9 +13,12 @@ router.get("/:keyword", async (req, res) => {
     const shops = await Shop.find({
       keyword,
     });
+    //   가봤어요 true false => 리뷰안에 유저아이디 / 샵아이디 있으면 true : false
+    // 리뷰들 rated 카테고리별 개수 전달
     //   키워드로 찾을 수 없음 ID값으로 찾기
     if (shops.length === 0) {
       const shop = await Shop.findById(keyword);
+
       console.log(shop);
       res.json({ shop });
     }
@@ -21,6 +26,7 @@ router.get("/:keyword", async (req, res) => {
     else if (shops.length > 1) {
       const shopList = shops.map((s) => ({
         shopName: s.shopName,
+        shopId: s.shopId,
         keyword: s.keyword,
         star: s.star,
         address: s.data[0][1],
