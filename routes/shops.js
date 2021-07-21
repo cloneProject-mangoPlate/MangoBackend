e15d6ecce28a00fecde8c3889d6a6139c3da9228
soujinko <soujinko@naver.com>
2021-07-21 16:45:25 +0900

    update shop.js

--- routes/shops.js
+++ routes/shops.js
@@ -1,13 +1,14 @@
 import express from "express";
 import Shop from "../models/shop.js";
-
+import ObjectID from "bson-objectid";
 const router = express.Router();
 
 router.get("/", async (req, res) => {});
 
-router.get("/:keyword", async (req, res) => {
+router.get("/:keyword/:userId", async (req, res) => {
   try {
-    const { keyword } = req.params;
+    // const { userId } = res.locals
+    const { keyword, userId } = req.params;
     console.log(req.params);
 
     const shops = await Shop.find({
@@ -17,7 +18,25 @@ router.get("/:keyword", async (req, res) => {
     // 리뷰들 rated 카테고리별 개수 전달
     //   키워드로 찾을 수 없음 ID값으로 찾기
     if (shops.length === 0) {
-      const shop = await Shop.findById(keyword);
+      let shop = await Shop.findById(keyword)
+        .populate("reviews")
+        .sort("-created_at");
+
+      // 유저 방문 여부 판단
+      let visits = shop.reviews.map((s) => s.userIds.join());
+      shop.visited = visits.includes(userId) ? true : false;
+
+      // 점수 카테고리별 개수
+      const rates = { highsRate: 0, middleRate: 0, lowsRate: 0 };
+      shop.reviews.forEach((e) => {
+        if (e.rate === "맛있다") {
+          rates.highsRate += 1;
+        } else if (e.rate === "괜찮다") {
+          rates.middleRate += 1;
+        } else if (e.rate === "별로") {
+          rates.lowsRate += 1;
+        }
+      });
 
       console.log(shop);
       res.json({ shop });
