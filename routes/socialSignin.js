16c64d2ac4a532c44167c63d0bf28ab9ba682aa2
soujinko <soujinko@naver.com>
2021-07-21 16:45:48 +0900

    update shocialSignin

--- routes/socialSignin.js
+++ routes/socialSignin.js
@@ -3,25 +3,20 @@ import User from "../models/user.js";
 import passport from "passport";
 import request from "request";
 import jwt from "jsonwebtoken";
-<<<<<<< HEAD
 import dotenv from "dotenv";
 dotenv.config();
-=======
-import dotenv from 'dotenv'
->>>>>>> ba029d8f7a98bc73c38d3d1254f9358d87d99244
 // strategy import
 import Kakao from "passport-kakao";
 
-dotenv.config()
 const router = express.Router();
 router.use(passport.initialize());
 const KakaoStrategy = Kakao.Strategy;
 
 // 카카오 로그아웃
-router.get("/signout", function (req, res) {
-  req.logout();
-  res.redirect("/");
-});
+// router.get("/signout", function (req, res) {
+//   req.logout();
+//   res.redirect("/");
+// });
 
 // 카카오로그인
 passport.serializeUser(function (user, done) {
