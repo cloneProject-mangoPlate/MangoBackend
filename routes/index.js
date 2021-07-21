4c09600e307070a6f2741801a379dd920f60a732
soujinko <soujinko@naver.com>
2021-07-21 16:46:08 +0900

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,14 @@ import auth from "../middlewares/auth-middleware.js";
 import shopsRouter from "./shops.js";
 import reviewsRouter from "./reviews.js";
 import searchRouter from "./search.js";
-<<<<<<< HEAD
-import signinRouter from "./socialSignin.js";
-=======
-import socialRouter from "./socialSignin.js"
->>>>>>> ba029d8f7a98bc73c38d3d1254f9358d87d99244
+import socialRouter from "./socialSignin.js";
 
 const router = express.Router();
 
 router.use("/shops", shopsRouter);
 router.use("/reviews", reviewsRouter);
 router.use("/search", searchRouter);
-<<<<<<< HEAD
-router.use("/social", signinRouter);
-router.use(auth);
-=======
 router.use("/social", socialRouter);
 // router.use(auth);
->>>>>>> ba029d8f7a98bc73c38d3d1254f9358d87d99244
 
 export default router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //리뷰 작성하기
 router.post('/:shopId', async(req, res) => {
     const { shopId } = req.params;
-    const { nickname, profilePic, text, rate, userId} = req.body;
+    const { profilePic, text, rate, userId} = req.body;
     // const { userId } = res.locals
     if (text === '') {
         res.status(400).send({
@@ -17,7 +17,7 @@ router.post('/:shopId', async(req, res) => {
       }
 
     try{
-    const review = new Review({ shopId, userId, nickname, profilePic, text, rate })
+    const review = new Review({ shopId, userId, profilePic, text, rate })
     const shop = await Shop.findById(shopId) 
     await review.save(async function (){
         try{
