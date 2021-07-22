6519b1e25df2839a40e5ee7dfb58f98271347477
soujinko <soujinko@naver.com>
2021-07-22 16:38:28 +0900

    no message

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,39 +1,39 @@
-import User from "../models/user.js";
+// import User from "../models/user.js";
 
-export default (req, res, next) => {
-  const { authorization } = req.headers;
+// export default (req, res, next) => {
+//   const { authorization } = req.headers;
 
-  if (!authorization){
-    res.status(401).send({
-      errorMessage: "token이 없습니다"
-    })
-    return;
-  }
+//   if (!authorization){
+//     res.status(401).send({
+//       errorMessage: "token이 없습니다"
+//     })
+//     return;
+//   }
 
-  const [tokenType, tokenValue] = authorization.split(" ");
+//   const [tokenType, tokenValue] = authorization.split(" ");
 
-  if (tokenType !== "Bearer") {
-    res.sendStatus(401)
-    return;
-  }
+//   if (tokenType !== "Bearer") {
+//     res.sendStatus(401)
+//     return;
+//   }
 
-  try {
-    const { userId } = jwt.verify(tokenValue, process.env.SECRET_KEY);
+//   try {
+//     const { userId } = jwt.verify(tokenValue, process.env.SECRET_KEY);
 
-    User.findById(userId).then((user) => {
-      if (user === null) {
-        throw new Error("invalidUser");
-      }
-      res.locals.userId = userId;
-      next();
-    });
-  } catch (err) {
-    if (err.name === "TokenExpiredError") {
-      res.status(419).send({ message: "token 만료" });
-      return;
-    } else {
-      res.status(401).send({ message: "token이 유효하지 않습니다." });
-      return;
-    }
-  }
-};
+//     User.findById(userId).then((user) => {
+//       if (user === null) {
+//         throw new Error("invalidUser");
+//       }
+//       res.locals.userId = userId;
+//       next();
+//     });
+//   } catch (err) {
+//     if (err.name === "TokenExpiredError") {
+//       res.status(419).send({ message: "token 만료" });
+//       return;
+//     } else {
+//       res.status(401).send({ message: "token이 유효하지 않습니다." });
+//       return;
+//     }
+//   }
+// };
