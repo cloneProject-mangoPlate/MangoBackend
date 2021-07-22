110040d8b91c1a8c0cb7e7303904b7ca5d3ba965
soujinko <soujinko@naver.com>
2021-07-22 12:18:08 +0900

    nothing changed

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import auth from "../middlewares/auth-middleware.js";
+// import auth from "../middlewares/auth-middleware.js";
 import shopsRouter from "./shops.js";
 import reviewsRouter from "./reviews.js";
 import searchRouter from "./search.js";
